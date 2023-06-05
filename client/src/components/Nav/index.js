f742c9480054b3466b3eaf317f5feb0008ea9f3f
rgiinther <r.giin95@gmail.com>
2023-06-04 22:10:17 -0500

    updated dropdowm choices

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -5,11 +5,17 @@ function Nav () {
         <header className="bg-gray-800 md:sticky top-0 z-10">
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
             <a className="title-font font-medium text-white mb-4 md:mb-0">
-              <a className="ml-3 text-xl" href="#about">Niche Stitch Co.</a>
+              <a className="ml-3 text-xl" href="#about">North Shore Fibre Company</a>
             </a>
           <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-            <a href="#skills" className="mr-5 text-white hover:text-slate-400">Patterns</a>
-            <a href="#projects" className="mr-5 text-white hover:text-slate-400">Shop</a>
+              <label> Shop
+                <select>
+                  <option value = "Patterns"> Patterns</option>
+                  <option value = "Yarn">Yarn</option>
+                </select>
+              </label>
+      
+  
           </nav>
           <a href="#contact" className="inline-flex text-white items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
             More
